72cfa36cffab44f36e3f525f5439590a791e3d04
Everett Vergara <evergara@shinra.com.ph>
2025-10-01 19:53:30 +0800

    added up to 6 auto dialers

--- AD3/WMDIParentFrameAD3.hpp
+++ AD3/WMDIParentFrameAD3.hpp
@@ -4,6 +4,7 @@
 
 #include "WConfigSettings.hpp"
 #include "WDialer.h"
+//#include "WDialer2.h"
 #include "resource.h"
 
 namespace eg::ad3
@@ -26,7 +27,13 @@ namespace eg::ad3
 			register_sub_menu(file_menu, next_menu_id(), "Close", &WMDIParentFrameAD3::on_file_close, this);
 
 			auto* call_menu = new wxMenu();
-			register_sub_menu(call_menu, next_menu_id(), "Dialer", &WMDIParentFrameAD3::on_call_dialer, this);
+			register_sub_menu(call_menu, next_menu_id(), "Dialer - 1", &WMDIParentFrameAD3::on_call_dialer1, this);
+			register_sub_menu(call_menu, next_menu_id(), "Dialer - 2", &WMDIParentFrameAD3::on_call_dialer2, this);
+			register_sub_menu(call_menu, next_menu_id(), "Dialer - 3", &WMDIParentFrameAD3::on_call_dialer3, this);
+			register_sub_menu(call_menu, next_menu_id(), "Dialer - 4", &WMDIParentFrameAD3::on_call_dialer4, this);
+			register_sub_menu(call_menu, next_menu_id(), "Dialer - 5", &WMDIParentFrameAD3::on_call_dialer5, this);
+			register_sub_menu(call_menu, next_menu_id(), "Dialer - 6", &WMDIParentFrameAD3::on_call_dialer6, this);
+
 			//register_sub_menu(call_menu, next_menu_id(), "Auto dial", &WMDIParentFrameAD3::on_call_auto_dial, this);
 
 			menu->Append(file_menu, "&File");
@@ -43,9 +50,9 @@ namespace eg::ad3
 			}
 		}
 
-		void on_call_dialer(wxCommandEvent&)
+		void on_call_dialer1(wxCommandEvent&)
 		{
-			static constexpr auto id = "dialer";
+			static constexpr auto id = "dialer1";
 			if (not is_child_active(id))
 			{
 				auto child = new WDialer(this);
@@ -55,6 +62,65 @@ namespace eg::ad3
 			}
 		}
 
+		void on_call_dialer2(wxCommandEvent&)
+		{
+			static constexpr auto id = "dialer2";
+			if (not is_child_active(id))
+			{
+				auto child = new WDialer(this);
+				child_ids[id] = child;
+
+				return;
+			}
+		}
+
+		void on_call_dialer3(wxCommandEvent&)
+		{
+			static constexpr auto id = "dialer3";
+			if (not is_child_active(id))
+			{
+				auto child = new WDialer(this);
+				child_ids[id] = child;
+
+				return;
+			}
+		}
+
+		void on_call_dialer4(wxCommandEvent&)
+		{
+			static constexpr auto id = "dialer4";
+			if (not is_child_active(id))
+			{
+				auto child = new WDialer(this);
+				child_ids[id] = child;
+
+				return;
+			}
+		}
+
+		void on_call_dialer5(wxCommandEvent&)
+		{
+			static constexpr auto id = "dialer5";
+			if (not is_child_active(id))
+			{
+				auto child = new WDialer(this);
+				child_ids[id] = child;
+
+				return;
+			}
+		}
+
+		void on_call_dialer6(wxCommandEvent&)
+		{
+			static constexpr auto id = "dialer6";
+			if (not is_child_active(id))
+			{
+				auto child = new WDialer(this);
+				child_ids[id] = child;
+
+				return;
+			}
+		}
 		void on_file_close(wxCommandEvent&)
 		{
 			Close(true);
